43270de6dd967c5c2813d6cd89eb77318939772c
Antonio_Brooks <Abrooks6211996@gmail.com>
2020-02-13 18:35:11 -0500

    GD205

---
+++ GD205_Project/Assets/Scenes/Script/W2Board.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class W2Board : MonoBehaviour
+{
+    public GameObject player;
+    //Vector3 initPos;
+
+    // Start is called before the first frame update
+    void Start() => transform.position.Set(1, 1, 1);
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            player.transform.position += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            player.transform.position += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+            player.transform.position += new Vector3(0, 0, -1);
+
+        if (Input.GetKeyDown(KeyCode.A))
+            player.transform.position += new Vector3(-1, 0, 0);
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+
+        {
+           player.transform.position += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            player.transform.position += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+
+            player.transform.position += new Vector3(0, 0, -1);
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+
+            player.transform.position += new Vector3(-1, 0, 0);
+    }
+}
