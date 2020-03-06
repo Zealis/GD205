063620501b3b8e4a162dcecf006d72f1f19acd8b
Reedbetweenthelines <marcusreed1324@gmail.com>
2020-03-05 20:13:04 -0500

    Sum

---
+++ Assets/Script/PrayBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrayBehavior : MonoBehaviour
+{
+    Rigidbody rb;
+    public Transform predator;
+    public float terrorForce = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 predatorDirection = Vector3.Normalize(predator.position - transform.position);
+
+        rb.AddForce(-predatorDirection * terrorForce);
+
+    }
+}
---
+++ Assets/Script/Thrust.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thrust : MonoBehaviour
+{
+    Rigidbody rb;
+    //public GameObject victim;
+    //public GameObject anothervictim;
+    public string predatorTag , preyTag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(transform.forward * 5f);
+        }
+    }
+
+    void OnCollisionEnter(Collision myCollision)
+    {
+
+        if (myCollision.gameObject.CompareTag(preyTag))
+
+        //if (myCollision.gameObject == victim)
+        {
+            Destroy(myCollision.gameObject);
+        }
+        else if (myCollision.gameObject.CompareTag(predatorTag))
+            Destroy(gameObject);
+
+       // if(myCollision.gameObject == anothervictim){
+           // Destroy(anothervictim);
+       // }
+    }
+}
